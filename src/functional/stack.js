3ae8b33a146788fe3279f5ff66bebfdcc9a047a9
Hack Reactor Students <github@hackreactor.com>
2013-08-07 14:59:15 -0700

    passing 'report size correctly' and 'pop items in LIFO order' tests.

--- src/functional/stack.js
+++ src/functional/stack.js
@@ -2,18 +2,27 @@ var makeStack = function(){
   // Use an object with numeric keys to store values
   var storage = {};
 
-  var size; // Hint: set an initial value here
+  var size = 0; // Hint: set an initial value here
 
   // Implement the methods below
   var stack = {};
 
   stack.push = function(value){
+    storage[size] = value;
+    size++;
   };
 
   stack.pop = function(){
+    var result;
+    if(size){
+      size--;
+    }
+    result = storage[size];
+    return result;
   };
 
   stack.size = function(){
+    return size;
   };
 
   return stack;
