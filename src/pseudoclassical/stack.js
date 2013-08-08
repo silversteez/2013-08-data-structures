bbffe5c23bec294e9643b45f6347c4bc8b351df3
Hack Reactor Students <github@hackreactor.com>
2013-08-07 20:30:22 -0700

    completed pseudoclassical stack.js, passing all tests

--- src/pseudoclassical/stack.js
+++ src/pseudoclassical/stack.js
@@ -1,3 +1,27 @@
-var Stack = function() {
-  // Hey! Copy your code from src/prototypal/stack.js and paste it here
+var Stack = function(){
+  // Use an object with numeric keys to store values
+ // Hint: set an initial value here
+  // Implement the methods below
+  this.storage = {};
+  this.stackSize = 0;
 };
+
+Stack.prototype.push = function(value){
+  this.storage[this.stackSize] = value;
+  this.stackSize++;
+};
+
+Stack.prototype.pop = function(){
+  var result;
+  if(this.stackSize){
+    this.stackSize--;
+  }
+  result = this.storage[this.stackSize];
+  return result;
+};
+
+Stack.prototype.size = function(){
+  return this.stackSize;
+};
+
+var newStack = new Stack();
