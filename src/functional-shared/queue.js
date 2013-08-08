a2b74b3c9598ebb5e202296552476b2747a02b0d
Hack Reactor Students <github@hackreactor.com>
2013-08-07 17:03:26 -0700

    modified queue.js to include shared methods. passing all tests.

--- src/functional-shared/queue.js
+++ src/functional-shared/queue.js
@@ -1,3 +1,34 @@
 var makeQueue = function(){
-	// Hey! Copy your code from src/functional/queue.js and paste it here
+  // Use an object with numeric keys to store values
+
+  // Implement the methods below
+  var queue = {
+    storage : {},
+    first : 0,
+    last : 0
+  };
+
+
+  _.extend(queue, queueMethods);
+
+  return queue;
+};
+
+var queueMethods = {
+  enqueue : function(value){
+    this.storage[this.last] = value;
+    this.last++;
+  },
+
+  dequeue : function(){
+    var result = this.first;
+    if(this.first < this.last){
+      this.first++;
+    }
+    return this.storage[result];
+  },
+
+  size : function(){
+    return this.last - this.first;
+  }
 };
