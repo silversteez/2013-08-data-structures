4f0d39ee0140c9fc95806dc8a0664fee16b9e516
Hack Reactor Students <github@hackreactor.com>
2013-08-07 20:08:36 -0700

    completed prototypal version of stack.js, passing all tests

--- src/prototypal/stack.js
+++ src/prototypal/stack.js
@@ -1,3 +1,30 @@
-var Stack = function() {
-  // Hey! Copy your code from src/functional-shared/stack.js and paste it here
+var Stack = function(){
+  // Use an object with numeric keys to store values
+ // Hint: set an initial value here
+  // Implement the methods below
+  var stack = Object.create(stackMethods);
+  stack.storage = {};
+  stack.stackSize = 0;
+
+  return stack;
+};
+
+var stackMethods = {
+  push : function(value){
+    this.storage[this.stackSize] = value;
+    this.stackSize++;
+  },
+
+  pop : function(){
+    var result;
+    if(this.stackSize){
+      this.stackSize--;
+    }
+    result = this.storage[this.stackSize];
+    return result;
+  },
+
+  size : function(){
+    return this.stackSize;
+  }
 };
