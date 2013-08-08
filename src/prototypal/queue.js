c454270c04858712f8aeac9d8d51c9ec80d22a8f
Hack Reactor Students <github@hackreactor.com>
2013-08-07 20:03:09 -0700

    completed prototypal version of queue.js, passing all tests

--- src/prototypal/queue.js
+++ src/prototypal/queue.js
@@ -1,3 +1,30 @@
-var Queue = function() {
-  // Hey! Copy your code from src/functional-shared/queue.js and paste it here
+var Queue = function(){
+  // Use an object with numeric keys to store values
+
+  // Implement the methods below
+  var queue = Object.create(queueMethods);
+    queue.storage = {};
+    queue.first = 0;
+    queue.last = 0;
+
+  return queue;
+};
+
+var queueMethods = {
+  enqueue : function(value){
+    this.storage[this.last] = value;
+    this.last++;
+  },
+
+  dequeue : function(){
+    var result = this.first;
+    if(this.first < this.last){
+      this.first++;
+    }
+    return this.storage[result];
+  },
+
+  size : function(){
+    return this.last - this.first;
+  }
 };
