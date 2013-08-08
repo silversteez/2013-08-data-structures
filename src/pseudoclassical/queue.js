e519837a96693c6d8d43a629f3fa16eacfc033a9
Hack Reactor Students <github@hackreactor.com>
2013-08-07 20:21:20 -0700

    completed pseudoclassical queue.js, passing all tests

--- src/pseudoclassical/queue.js
+++ src/pseudoclassical/queue.js
@@ -1,3 +1,27 @@
-var Queue = function() {
-  // Hey! Copy your code from src/prototypal/queue.js and paste it here
+var Queue = function(){
+  // Use an object with numeric keys to store values
+
+  // Implement the methods below
+  this.storage = {};
+  this.first = 0;
+  this.last = 0;
 };
+
+Queue.prototype.enqueue = function(value){
+  this.storage[this.last] = value;
+  this.last++;
+};
+
+Queue.prototype.dequeue = function(){
+  var result = this.first;
+  if(this.first < this.last){
+    this.first++;
+  }
+  return this.storage[result];
+};
+
+Queue.prototype.size = function(){
+  return this.last - this.first;
+};
+
+var newQueue = new Queue();
