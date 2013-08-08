ff2357a863928feb862c2496a02aacc9b3c286b3
Hack Reactor Students <github@hackreactor.com>
2013-08-07 17:14:55 -0700

    stack shares methods and totally works

--- src/functional-shared/stack.js
+++ src/functional-shared/stack.js
@@ -1,3 +1,33 @@
 var makeStack = function(){
-  // Hey! Copy your code from src/functional/stack.js and paste it here
+  // Use an object with numeric keys to store values
+ // Hint: set an initial value here
+  // Implement the methods below
+  var stack = {
+    storage : {},
+    stackSize : 0
+  };
+
+  _.extend(stack, stackMethods);
+
+  return stack;
+};
+
+var stackMethods = {
+  push : function(value){
+    this.storage[this.stackSize] = value;
+    this.stackSize++;
+  },
+
+  pop : function(){
+    var result;
+    if(this.stackSize){
+      this.stackSize--;
+    }
+    result = this.storage[this.stackSize];
+    return result;
+  },
+
+  size : function(){
+    return this.stackSize;
+  }
 };
